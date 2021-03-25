a7e25fedd21aae4f17eb1d64654be70b287f8055
Joseph Itiose <rexgmann@gmail.com>
2021-03-25 02:20:40 +0100

    New Message

--- index.js
+++ index.js
@@ -150,3 +150,75 @@ const app = express()
       return price;
     
   }
+
+  app.get("/math", (req, res) => {
+    var result = solveMath(req);
+    res.render("pages/results", {
+      answer: result
+    });
+  });
+  app.get("/math_service", (req, res) => {
+  
+    res.writeHead(200, {
+      "Content-Type": "application/json"
+    })
+    var result = solveMath(req);
+    console.log(result);
+    var json = JSON.stringify({
+      "answer": result
+    })
+    console.log(`math service: ${json}`);
+    res.end(json);
+  })
+  // async function () {
+  //   let response = await fetch('/math_service')
+  //   let responseJson = await response.json()
+  //   let fromServer = responseJson.myString
+  //   alert(fromServer)
+  // }
+  // app.get(fetch('/results')
+  //   .then(response => response.json())
+  //   .then(data => console.log(data));
+  //   componentWillMount: function(){
+  //     var fromServer = fetch('/results')
+  //     .then(function(response) {
+  //       return response.json()
+  //     })
+  //     .then(function(responseJson) {
+  //       return responseJson.myString
+  //     })
+  //     alert(fromServer);
+  //   },
+  app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+  // if a file is in the public it just renders automatically
+  // operand1, operation, operand2
+  function solveMath(req) {
+    var operand1 = req.query.operand1;
+    var operand2 = req.query.operand2;
+    var operation = req.query.operation;
+  
+    if (operation == "+") {
+      var result = +operand1 + +operand2;
+  
+    }
+    if (operation == "-") {
+      var result = +operand1 - +operand2;
+  
+    }
+    if (operation == "*") {
+      var result = +operand1 * +operand2;
+  
+    }
+    if (operation == "/") {
+      var result = +operand1 / +operand2;
+  
+    }
+    console.log(operation);
+    console.log(operand2);
+    console.log(operand1);
+    console.log(result);
+    // res.render('/results', () => {
+    //   answer: result 
+    // })
+    return result;
+  }
